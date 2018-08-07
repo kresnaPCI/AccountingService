455dcf238c6b151c2ad9ffb0809dea10deb14653
Alex Stansfield <alex.stansfield@gmail.com>
2018-08-07 13:54:01 +0700

    Added Credit Memo Code

--- src/AccountingPlatform/CreditMemo/OdooAdapter.php
+++ src/AccountingPlatform/CreditMemo/OdooAdapter.php
@@ -28,19 +28,114 @@ class OdooAdapter implements AdapterInterface
 
     public function create(CreditMemo $creditMemo): bool
     {
-        // TODO: Implement create() method.
+        $invoice = $this->odooClient->search_read(
+            'account.invoice',
+            [['id', '=', $creditMemo->getInvoiceId()], ['state', 'not in', ['draft', 'cancel']]]
+        );
+
+        if (empty($invoice)) {
+            return false;
+        }
+        $this->odooClient->methods('account.invoice', 'refund', $invoice[0]['id']);
         return true;
     }
 
     public function updateDate(string $accountId, int $creditMemoId, DateTime $date, string $pdfLink): bool
     {
         // TODO: Implement updateDate() method.
+        $update_invoice_date = $date->format('Y-m-d');
+        $data = [
+            'date_invoice' => $update_invoice_date,
+        ];
+        $this->odooClient->write('account.invoice', $creditMemoId, $data);
         return true;
     }
 
-    public function markPaid(string $accountId, int $creditMemoId, string $method, string $transactionId, string $pdfLink): bool
-    {
-        // TODO: Implement markPaid() method.
+    public function markPaid(
+        string $accountId,
+        int $creditMemoId,
+        string $method,
+        string $transactionId,
+        string $pdfLink
+    ): bool {
+        $data = [
+            'accountId' => $accountId,
+            'creditMemoId' => $creditMemoId,
+            'status' => $status,
+            'transactionId' => $transactionId,
+            'pdfLink' => $pdfUrl,
+            'paymentType' => $paymentType,
+            'partnerType' => $partnerType
+        ];
+
+        // ============================= SEARCH DATA INVOICE ==============================
+        $fields = ['id', 'number', 'partner_id', 'account_id', 'date_invoice', 'journal_id', 'state', 'amount_total'];
+
+        $invoice = $this->odooClient->search_read(
+            'account.invoice',
+            [['id', '=', $creditMemoId]],
+            $fields,
+            1
+        );
+        $this->odooClient->methods(
+            'account.invoice',
+            'action_invoice_open',
+            $invoice[0]['id']
+        );
+        $invoice = $this->odooClient->search_read(
+            'account.invoice',
+            [['id', '=', $creditMemoId]],
+            $fields,
+            1
+        );
+
+        $invoice_partner_id = $invoice[0]['partner_id'][0];
+        $invoice_account_id = $invoice[0]['account_id'][0];
+        $invoice_journal_id = $invoice[0]['journal_id'][0];
+        $invoice_amount = $invoice[0]['amount_total'];
+        $invoice_number = $invoice[0]['number'];
+        $payment_date = date('Y-m-d'); # now
+
+        // # ============================= SEARCH DATA ACCOUNT JOURNAL ==============================
+        $payment_journal = $this->odooClient->search_read(
+            'account.journal',
+            [['id', '=', $data['transactionId']],], ['id', 'name', 'inbound_payment_method_ids'],
+            1
+        );
+        file_put_contents(
+            'markpaid_payment_method.txt',
+            print_r($payment_journal, true) . PHP_EOL,
+            FILE_APPEND | LOCK_EX
+        );
+        $payment_method = $payment_journal[0]['inbound_payment_method_ids'][0];
+
+        // # ============================= CREATE PAYMENT ==============================
+        $data_payment = [
+            'payment_date' => $payment_date,
+            'payment_method_id' => $payment_method,
+            'communication' => $invoice_number,
+            'invoice_ids' => array(array(4, $invoice[0]['id'], 0)),
+            'amount' => $invoice_amount,
+            'payment_type' => $data['paymentType'],
+            'partner_type' => $data['partnerType'],
+            'partner_id' => $invoice_partner_id,
+            'journal_id' => $accountId,
+        ];
+        $payment = $this->odooClient->create('account.payment', $data_payment);
+
+        $this->odooClient->methods(
+            'account.payment',
+            'action_validate_invoice_payment',
+            $payment
+        );
+        $payment_account = $this->odooClient->search_read('account.payment', [['id', '=', $payment],]);
+
+        $this->odooClient->methods(
+            'account.payment',
+            'post',
+            $payment_account[0]['id']
+        );
+
         return true;
     }
 
@@ -52,7 +147,17 @@ class OdooAdapter implements AdapterInterface
 
     public function markCancelled(string $accountId, int $creditMemoId, string $pdfLink): bool
     {
-        // TODO: Implement markCancelled() method.
+        $creditMemo = $this->odooClient->search_read(
+            'account.invoice',
+            [['id', '=', $creditMemoId], ['state', 'in', ['open', 'draft']]]
+        );
+
+        if (empty($creditMemo)) {
+            return false;
+        }
+
+        $this->odooClient->methods('account.invoice', 'action_invoice_cancel', $creditMemo[0]['id']);
+
         return true;
     }
 }
